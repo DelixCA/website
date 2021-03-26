948b39c52380c9032a7cf692f96bd907961d3b70
Aditya Patil <73546596+AdityaCyberSafe@users.noreply.github.com>
2021-03-26 19:08:10 +0000

    Delete theme.js

--- js/theme.js
+++
@@ -1,6 +0,0 @@
-<script type="text/javascript">
-  // Wait for document to load
-  document.addEventListener("DOMContentLoaded", function(event) {
-    document.documentElement.setAttribute("data-theme", "dark");
-  });
-</script>
