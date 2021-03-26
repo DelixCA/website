95900ddc5ccfa7aa8f029bf94167a72323dcf091
Aditya Patil <73546596+AdityaCyberSafe@users.noreply.github.com>
2021-03-26 19:07:39 +0000

    Create theme.js

---
+++ js/theme.js
@@ -0,0 +1,6 @@
+<script type="text/javascript">
+  // Wait for document to load
+  document.addEventListener("DOMContentLoaded", function(event) {
+    document.documentElement.setAttribute("data-theme", "dark");
+  });
+</script>
